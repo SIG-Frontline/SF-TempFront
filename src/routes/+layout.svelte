1a0f620125f47250d45b2e398a9e35111bc685b4
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-06-16 14:20:03 -0400

    Added auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
     import "./styles.css";
     import logo from "$lib/images/logo.webp";
     import { goto } from "$app/navigation";
+    import { SignIn, SignOut } from "@auth/sveltekit/components";
+    import { page } from "$app/stores";
 
     function routeTo(route, replaceState) {
         goto(`/${route}`, { replaceState });
@@ -15,7 +17,35 @@
         <a href="/"><p>Home</p></a>
         <a href="/wip/sb"><p>Schedule Builder</p></a>
         <a href="/wip/ra"><p>Room Availability</p></a>
+
+        <div>
+            {#if $page.data.session}
+                {#if $page.data.session.user?.image}
+                    <img
+                        src={$page.data.session.user.image}
+                        class="avatar rounded"
+                        alt="User Avatar"
+                    />
+                {/if}
+                <span class="signedInText">
+                    <small>Signed in as</small><br />
+                    <strong>{$page.data.session.user?.name ?? "User"}</strong>
+                </span>
+                <SignOut>
+                    <div slot="submitButton" class="buttonPrimary">
+                        Sign out
+                    </div>
+                </SignOut>
+            {:else}
+                <span class="notSignedInText">You are not signed in</span>
+                <SignIn>
+                    <div slot="submitButton" class="buttonPrimary">Sign in</div>
+                </SignIn>
+                <SignIn provider="Auth0" />
+            {/if}
+        </div>
     </div>
+
     <div class="sidebar-left"></div>
     <div class="site-body"><slot></slot></div>
     <div class="sidebar-right"></div>
