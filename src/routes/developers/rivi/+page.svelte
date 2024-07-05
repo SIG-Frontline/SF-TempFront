1387f5554a15c7f139a83b77e517ac8635b8130d
iarivera <iar3@njit.edu>
2024-07-05 16:38:52 -0400

    i forget to do this first lmao

---
+++ src/routes/developers/rivi/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    import flag from "$lib/images/enby-flag.jpg";
+</script>
+
+<div class="rivi-body">
+    <h1>Rivi's Page</h1>
+    <p>
+        Hi there, the name's Rivi (people call me that), I'm a non-binary
+        Computer Science major
+    </p>
+    <img src={flag} alt="Non-Binary Flag, colored (from top to bottom: Yellow, White, Purple, and Black"/>
+    <p>I've been a member of the local ACM chapter at NJIT. When I'm not reading up
+        different programming lanuages and DSA, I'm unwinding playing video games or
+        watching anime (I love Spy X Family and Dragon Ball)
+    </p>
+    <p>SIG Frontline existing is something I'm a fan of because it gives students
+        an opportunity to work on projects, and use tools they wouldn't normally learn
+        in class. Here's to hoping SIG Frontline keeps going and works on bigger and
+        better things :3
+    </p>
+</div>
