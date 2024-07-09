ae8f94b9b8ad266b3c2be748dea29e7c78aee64f
Johnny <jmnaze123@gmail.com>
2024-07-09 10:04:55 -0400

    init

---
+++ src/routes/johnny/+layout.svelte
@@ -0,0 +1,19 @@
+<link rel="preconnect" href="https://rsms.me/" />
+<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+
+<div class="mb-12 mt-8 flex w-dvw flex-col items-center gap-8 px-80" id="container">
+	<slot />
+</div>
+
+<style>
+	:root {
+		background-color: transparent !important;
+	}
+
+	#container {
+		font-family: Inter, monospace !important;
+		font-feature-settings:
+			'liga' 1,
+			'calt' 1 !important; /* fix for Chrome */
+	}
+</style>
---
+++ src/routes/johnny/+page.svelte
@@ -0,0 +1,101 @@
+<div class="relative w-full">
+	<div class="absolute inset-0 -z-10 bg-fuchsia-500 opacity-20 blur-3xl"></div>
+	<h1 class="text-2xl font-semibold">Hi, I'm</h1>
+	<h1
+		class="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-8xl font-bold leading-tight text-transparent"
+	>
+		Johnny
+	</h1>
+</div>
+
+<div class="flex w-full flex-col gap-2">
+	<h2 class="text-2xl font-semibold">About</h2>
+	<p>
+		I'm a software engineer studying at the New Jersey Institute of Technology. I'm passionate about
+		building web apps and I love to learn new things. Currently, I'm practicing my skills with
+		React, Svelte, and Node.js.
+	</p>
+</div>
+
+<div class="flex w-full flex-col gap-2">
+	<h2 class="text-2xl font-semibold">Socials</h2>
+	<div class="flex w-full gap-4">
+		<a
+			href="https://github.com/johnnymayodev"
+			target="_blank"
+			class="my-auto flex h-fit w-fit justify-center rounded-lg bg-pink-400 p-3 text-inherit transition hover:scale-105 hover:bg-pink-300 active:scale-95"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="32"
+				height="100%"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+				/>
+			</svg>
+		</a>
+		<a
+			href="https://johnnymayo.com/"
+			target="_blank"
+			class="my-auto flex h-fit w-fit justify-center rounded-lg bg-pink-400 p-3 text-inherit transition hover:scale-105 hover:bg-pink-300 active:scale-95"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="32"
+				height="100%"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="icon icon-tabler icons-tabler-outline icon-tabler-world"
+				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+					d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"
+				/><path d="M3.6 9h16.8" /><path d="M3.6 15h16.8" /><path
+					d="M11.5 3a17 17 0 0 0 0 18"
+				/><path d="M12.5 3a17 17 0 0 1 0 18" /></svg
+			>
+		</a>
+	</div>
+</div>
+
+<div class="flex w-full flex-col gap-2">
+	<h2 class="text-2xl font-semibold">Projects</h2>
+	<div class="flex w-full flex-col gap-1">
+		<div class="flex h-72 w-full flex-col justify-between rounded-xl bg-pink-400 px-6 py-4">
+			<div class="flex flex-col gap-2">
+				<div>
+					<h1 class="text-4xl font-bold">Web Portfolio</h1>
+					<h2 class="text-lg font-semibold text-neutral-200">Solo Project</h2>
+				</div>
+				<p class="font-medium">
+					A simple website to display my projects. Made with HTML, CSS, JavaScript, and Python.
+				</p>
+			</div>
+			<div class="flex gap-4">
+				<a
+					href="https://johnnymayo.com/"
+					class="w-fit rounded-md bg-pink-300 px-4 py-2 text-inherit transition hover:scale-105 hover:bg-pink-200 active:scale-95"
+				>
+					Live Deployment
+				</a>
+				<a
+					href="https://github.com/johnnymayodev/portfolio/"
+					class="w-fit rounded-md bg-pink-300 px-4 py-2 text-inherit transition hover:scale-105 hover:bg-pink-200 active:scale-95"
+				>
+					View on GitHub
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
